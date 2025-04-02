c7e7ed9100217e0cd3dd71f87c73d7f9858e5d7b
SergeyNash <55077932+SergeyNash@users.noreply.github.com>
2025-04-02 17:23:27 +0300

    Update user-profile.tsx

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import Image from "/images/profile.jpg"
+import Image from "next/image"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import ReactMarkdown from "react-markdown"
 
